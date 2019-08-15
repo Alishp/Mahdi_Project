6e09bee47a3978dd772828f3c65a739af28ed991
Alirezashp <Alirezashahriaripour.72@gmail.com>
2019-08-15 14:54:03 +0430

    Update MainActivity.java

--- app/src/main/java/com/example/profileproject/MainActivity.java
+++ app/src/main/java/com/example/profileproject/MainActivity.java
@@ -7,13 +7,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import com.google.android.material.navigation.NavigationView;
-
 import java.util.ArrayList;
 import java.util.List;
 
